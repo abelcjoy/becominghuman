f3edb1c04d92e6bd8387128409705d1f08cac70e
abelcjoy <abelcjoy@gmail.com>
2025-07-22 17:09:28 +0530

    10 posts

--- posts.js
+++ posts.js
@@ -23,23 +23,23 @@ export const posts = [
         content: "Every time you choose to research more instead of start, you're training yourself to delay."
     },
     {
-        title: "The Gift of Self-Compassion",
-        content: "Treat yourself with the same kindness and understanding you would offer a dear friend. Self-compassion is not self-indulgence; it's a vital component of mental well-being."
+        title: "00006",
+        content: "People who meditate will know what Im talking about. Sometimes the ideas you get while meditating are so good you have to stop the session just to write them down."
     },
     {
-        title: "Mindful Eating",
-        content: "Pay attention to your food—its colors, textures, aromas, and flavors. Eating mindfully can enhance your appreciation for meals and help you feel more connected to your body's needs."
+        title: "00007",
+        content: "Dopamine is a neurotransmitter. it drives your motivation, focus, and pleasure. It spikes when you expect a reward But it also gets triggered by easy rewards like getting likes, messages, or new content."
     },
     {
-        title: "Digital Detox",
-        content: "Consider taking short breaks from screens. Disconnecting from digital noise can help clear your mind, reduce stress, and allow you to engage more fully with your immediate surroundings."
+        title: "00008",
+        content: "When you pass by a mirror, try not to make any internal statement about your appearance, whether good or bad. With little acts like this you will trick your ego into thinking that there are more important matters than your looks."
     },
     {
-        title: "The Importance of Sleep",
+        title: "00009",
         content: "Prioritize quality sleep. It's essential for physical and mental restoration. Create a relaxing bedtime routine to signal to your body that it's time to wind down."
     },
     {
-        title: "Set Healthy Boundaries",
-        content: "Learning to say 'no' can be empowering. Establishing clear boundaries protects your energy and time, allowing you to focus on what truly matters for your well-being."
+        title: "00010",
+        content: "Addictions can be a great reminder that there's something missing in our life."
     }
 ];
